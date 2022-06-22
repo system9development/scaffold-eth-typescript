3f060393c8996ddac71c4f285deacd70ad474888
Hart Simha <hmsimha@gmail.com>
2022-06-22 07:48:22 -0700

    Add lens deployment

---
+++ packages/hardhat-ts/deploy/08_deploy_lens.ts
@@ -0,0 +1,17 @@
+import { DeployFunction } from 'hardhat-deploy/types';
+import { THardhatRuntimeEnvironmentExtended } from 'helpers/types/THardhatRuntimeEnvironmentExtended';
+
+const func: DeployFunction = async (hre: THardhatRuntimeEnvironmentExtended) => {
+  const { getNamedAccounts, deployments } = hre;
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  await deploy('Lens', {
+    from: deployer,
+    log: true,
+    contract: 'CompoundLens',
+  });
+};
+
+export default func;
+func.tags = ['Lens'];

dca05a35f2aeb12ca5564745b660bf668a98c70d
Hart Simha <hmsimha@gmail.com>
2022-07-05 13:59:08 -0400

    Add basic Abis for Chainlink, CToken, ERc20

---
+++ packages/api/src/abis/ChainlinkOracle.js
@@ -0,0 +1,5 @@
+module.exports = JSON.stringify([
+  "function latestTimestamp() view returns (uint256)",
+  "function latestAnswer() view returns (int256)",
+  "function decimals() view returns (uint8)",
+]);
---
+++ packages/api/src/abis/Erc20Read.js
@@ -0,0 +1,6 @@
+module.exports = JSON.stringify([
+  "function name() view returns (string)",
+  "function symbol() view returns (string)",
+  "function decimals() view returns (uint8)",
+  "function balanceOf(address) view returns (uint256)",
+]);

cfaf07f9f26bd56e7de6811780b529e87aa3d957
Hart Simha <hmsimha@gmail.com>
2022-07-21 10:24:17 -0700

    Change Comp contract name back to Comp
    Calling it BDAMM causes problems in the Comptroller contract

--- packages/hardhat-ts/contracts/Governance/Comp.sol
+++ packages/hardhat-ts/contracts/Governance/Comp.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 pragma solidity ^0.8.10;
 
-contract BDAMM {
+contract Comp {
   /// @notice EIP-20 token name for this token
   string public constant name = "Bonded dAMM";
 

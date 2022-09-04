62007087fe44955f1e53cd63fd1775cbbcbc0ac6
Hart Simha <hmsimha@gmail.com>
2022-09-03 17:12:24 -0700

    Update blocks per year for PoS upgrade

--- packages/api/src/config.js
+++ packages/api/src/config.js
@@ -180,7 +180,7 @@ const uniswapV2PoolAddresses = {
   ETH_USDT: '0x0d4a11d5EEaaC28EC3F61d100daF4d40471f1852',
 };
 
-const BLOCKS_PER_YEAR = 2102400;
+const BLOCKS_PER_YEAR = 2629800;
 
 module.exports = {
   dammProvider,

00f4b9c63df589391bcfd9d5a97da3addd6349d8
Hart Simha <hmsimha@gmail.com>
2022-09-20 03:53:40 -0700

    Add json:update handler for chainalassist markets.json

--- packages/hardhat-ts/scripts/jsonupdate.ts
+++ packages/hardhat-ts/scripts/jsonupdate.ts
@@ -3,12 +3,41 @@ import '@nomiclabs/hardhat-ethers';
 import fs from 'fs';
 import path from 'path';
 
+import { CErc20 as ICErc20 } from 'generated/contract-types/CErc20';
 import { ethers } from 'hardhat';
 
 interface TokenData {
   [key: string]: {
     [key: string]: string
   }
+}
+
+/*
+  Chainalassist marketData is an array of objects that look like:
+[
+  {
+    "name": "dAAVE",
+    "configs": [
+      {
+        "chainId": 1,
+        "address": "0xA12949ED1eD88Fd8E32c96A153B14756F2c92178",
+        "underlying": "0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9"
+      },
+      ...
+    ]
+  },
+  ...
+]
+*/
+
+interface MarketConfig {
+  chainId: number;
+  address: string;
+  underlying: string;
+}
+interface MarketData {
+  name: string;
+  configs: MarketConfig[];
 }
 
 const getChainId = (): number => {
@@ -25,7 +54,7 @@ const getChainId = (): number => {
 const chainId = getChainId();
 const { FILE_ARG = '' } = process.env;
 
-const data: TokenData = JSON.parse(fs.readFileSync(FILE_ARG).toString());
+const data: TokenData | MarketData[] = JSON.parse(fs.readFileSync(FILE_ARG).toString());
 const baseName = path.basename(FILE_ARG);
 
 const updateCErcTokens: (data: TokenData) => Promise<TokenData> = async (data) => {
@@ -64,6 +93,30 @@ const updateTokens: (data: TokenData) => Promise<TokenData> = async (data) => {
   return data;
 }
 
+const updateChainalassistMarkets: (data: MarketData[]) => Promise<MarketData[]> = async (data) => {
+  for (let i = 0; i < data.length; i += 1) {
+    const marketData = data[i];
+    const marketContract = await ethers.getContractOrNull<ICErc20>(marketData.name);
+    if (marketContract !== null) {
+      const underlying = await marketContract.underlying();
+      const existingMarketConfig = marketData.configs.find((market) => market.chainId === chainId);
+      if (existingMarketConfig !== undefined) {
+        existingMarketConfig.address = marketContract.address;
+        existingMarketConfig.underlying = underlying;
+      } else {
+        marketData.configs.push({
+          chainId,
+          underlying,
+          address: marketContract.address,
+        });
+      }
+    } else {
+      console.error(`chainalassist markets config references ${marketData.name} but no contract found`);
+    }
+  }
+  return data;
+};
+
 const updateMain: (data: TokenData) => Promise<TokenData> = async (data) => {
   data.comptroller[chainId] = (await ethers.getContract('Unitroller')).address;
   data.oracle[chainId] = (await ethers.getContract('Oracle')).address;
@@ -72,17 +125,21 @@ const updateMain: (data: TokenData) => Promise<TokenData> = async (data) => {
 }
 
 if (baseName === 'tokens.json') {
-  updateTokens(data)
+  updateTokens(data as TokenData)
     .then((data) => console.log(JSON.stringify(data)))
     .catch((err) => console.error(err));
 } else if (baseName === 'main.json') {
-  updateMain(data)
+  updateMain(data as TokenData)
     .then((data) => console.log(JSON.stringify(data)))
     .catch((err) => console.error(err));
 } else if (baseName === 'cErcTokens.json') {
-  updateCErcTokens(data)
+  updateCErcTokens(data as TokenData)
     .then((data) => console.log(JSON.stringify(data)))
     .catch((err) => console.error(err));
+} else if (baseName === 'markets.json') {
+  updateChainalassistMarkets(data as MarketData[])
+    .then((data) => console.log(JSON.stringify(data)))
+    .catch((err) => console.error(err))
 } else {
   console.error(`file type ${baseName} not recognized`);
 }

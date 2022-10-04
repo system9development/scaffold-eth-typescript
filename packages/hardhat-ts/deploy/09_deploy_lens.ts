7580d1fed15f6bc3225b21e971cb91ff39eef9ed
Hart Simha <hmsimha@gmail.com>
2022-10-03 19:18:43 -0700

    Skip Lens deployment if already deployed

--- packages/hardhat-ts/deploy/09_deploy_lens.ts
+++ packages/hardhat-ts/deploy/09_deploy_lens.ts
@@ -1,3 +1,4 @@
+import { ethers } from 'hardhat';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { THardhatRuntimeEnvironmentExtended } from 'helpers/types/THardhatRuntimeEnvironmentExtended';
 
@@ -5,12 +6,16 @@ const func: DeployFunction = async (hre: THardhatRuntimeEnvironmentExtended) => 
   const { getNamedAccounts, deployments } = hre;
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
-
-  await deploy('Lens', {
-    from: deployer,
-    log: true,
-    contract: 'CompoundLens',
-  });
+  const existingLens = await ethers.getContractOrNull('Lens');
+  if (existingLens === null) {
+    await deploy('Lens', {
+      from: deployer,
+      log: true,
+      contract: 'CompoundLens',
+    });
+  } else {
+    console.log('Skipping Lens deployment; already deployed');
+  }
 };
 
 export default func;

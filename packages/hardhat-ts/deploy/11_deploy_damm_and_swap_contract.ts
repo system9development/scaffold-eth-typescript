9b74f4e818582ebc5d535710ee56b92b2eaa795e
Hart Simha <hmsimha@gmail.com>
2022-10-30 13:15:44 -0700

    Remove swap contract from deploy script

--- packages/hardhat-ts/deploy/11_deploy_damm_and_swap_contract.ts
+++ packages/hardhat-ts/deploy/11_deploy_damm_and_swap_contract.ts
@@ -1,5 +1,4 @@
 import { DAMM as IDAMM } from 'generated/contract-types/DAMM';
-import { DAMMProxy as IProxy } from 'generated/contract-types/DAMMProxy';
 import { ethers } from 'hardhat';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { THardhatRuntimeEnvironmentExtended } from 'helpers/types/THardhatRuntimeEnvironmentExtended';
@@ -14,24 +13,13 @@ const func: DeployFunction = async (hre: THardhatRuntimeEnvironmentExtended) => 
     contract: 'DAMM',
   });
   const dammImplementation = await ethers.getContract<IDAMM>('DAMM');
-  console.log('address', dammImplementation.address);
-  const damm = await deploy('DAMMProxy', {
+  await deploy('DAMMProxy', {
     from: deployer,
     log: true,
     contract: 'DAMMProxy',
     args: [
       ethers.utils.id('initialize()').substring(0, 10),
       dammImplementation.address,
-    ]
-  });
-  const bdamm = await ethers.getContract('BDAMM');
-  await deploy('Swap', {
-    from: deployer,
-    log: true,
-    contract: 'Swap',
-    args: [
-      bdamm.address,
-      damm.address
     ],
   });
 };

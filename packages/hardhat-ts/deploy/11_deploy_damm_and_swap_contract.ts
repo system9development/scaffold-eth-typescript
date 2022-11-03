971eaeb231531280c3b8c7080ab8b21a158e09ff
Hart Simha <hmsimha@gmail.com>
2022-11-03 13:55:59 -0700

    Update redemption contract for additional requirements:
    - constructor takes addresses for damm, bdamm, usdc, treasury,
      and an admin, as well as the initial USDC fee
    - All BDAMM and USDC sent for redemptions will be sent to the treasury
    - A running total of redemptions and USDC sent will be kept in storage
    - Events are emitted when a redemption occurs, and when the USDC fee is
      updated
    - Fix the redemption fee check

--- packages/hardhat-ts/deploy/11_deploy_damm_and_swap_contract.ts
+++ packages/hardhat-ts/deploy/11_deploy_damm_and_swap_contract.ts
@@ -1,7 +1,17 @@
 import { DAMM as IDAMM } from 'generated/contract-types/DAMM';
+import { Redemption as IRedemption } from 'generated/contract-types/Redemption';
 import { ethers } from 'hardhat';
 import { DeployFunction } from 'hardhat-deploy/types';
 import { THardhatRuntimeEnvironmentExtended } from 'helpers/types/THardhatRuntimeEnvironmentExtended';
+
+import {
+  mainnetTokens,
+  DAMM_TREASURY_ADDRESS,
+  DAMM_REDEMPTION_FEE_ADMIN,
+  CHAIN_ID as CHAIN_ID_ORIG,
+} from '../../api/src/config';
+
+// const CHAIN_ID = CHAIN_ID_ORIG === 31336 ? 1 : CHAIN_ID_ORIG;
 
 const func: DeployFunction = async (hre: THardhatRuntimeEnvironmentExtended) => {
   const { getNamedAccounts, deployments } = hre;
@@ -20,8 +30,33 @@ const func: DeployFunction = async (hre: THardhatRuntimeEnvironmentExtended) => 
     args: [
       ethers.utils.id('initialize()').substring(0, 10),
       dammImplementation.address,
+    ]
+  });
+  const dammProxy = await ethers.getContract<IDAMM>('DAMMProxy');
+  const damm = dammImplementation.attach(dammProxy.address);
+  const bdamm = await ethers.getContract('BDAMM');
+  const usdcAddress = (await ethers.getContractOrNull('USDC'))?.address ?? mainnetTokens.USDC.address;
+  await deploy('Redemption', {
+    from: deployer,
+    log: true,
+    contract: 'Redemption',
+    args: [
+      bdamm.address,
+      damm.address,
+      usdcAddress,
+      DAMM_REDEMPTION_FEE_ADMIN !== '' && typeof DAMM_REDEMPTION_FEE_ADMIN === 'string' ? DAMM_REDEMPTION_FEE_ADMIN : deployer,
+      DAMM_TREASURY_ADDRESS !== '' && typeof DAMM_TREASURY_ADDRESS === 'string' ? DAMM_TREASURY_ADDRESS : deployer,
+      ethers.utils.parseUnits('0.3', 6),
     ],
   });
+  if (CHAIN_ID_ORIG !== 1) {
+    // If we're not deploying to mainnet, save a step for testing and seed the redemption contract with 100 DAMM
+    const redemption = await ethers.getContract<IRedemption>('Redemption');
+    const usdcFeesCollected = await redemption.totalUSDCFees();
+    if (usdcFeesCollected.isZero()) {
+      await damm.transfer(redemption.address, ethers.utils.parseEther('100'));
+    }
+  }
 };
 
 export default func;
